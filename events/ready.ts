ca0a1417833e73dfaaf165e2ef2468ae3ad90a34
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2022-12-24 18:33:07 -0300

    feat: client presence

--- events/ready.ts
+++ events/ready.ts
@@ -1,10 +1,14 @@
-import { Client } from 'discord.js'
+import { ActivityType, Client } from 'discord.js'
 import { BotEvent } from '../types'
 
 const event: BotEvent = {
   name: 'ready',
   once: true,
   execute: (client: Client) => {
+    client.user?.setPresence({
+      activities: [{ name: 'Minecraft', type: ActivityType.Playing }],
+    })
+
     console.log(`[login] ${client.user?.tag}`)
   },
 }
